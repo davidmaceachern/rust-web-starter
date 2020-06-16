f0893f2926de4ec71b1ed3d12fa5ba54a3fc489c
David MacEachern <david.maceachern.tech@gmail.com>
2020-06-16 23:15:59 +0100

    Use state.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use dotenv;
 use kv_log_macro as log;
 
+use tide::Server;
+use tide::Request;
 use sqlx::Pool;
 use sqlx::PgPool;
 use sqlx::query;
@@ -8,18 +10,33 @@ use sqlx::query;
 #[async_std::main]
 async fn main() -> Result<(), Error>{    
     dotenv::dotenv().ok();
+
     femme::with_level(femme::LevelFilter::Trace);
     log::info!("Listening on port 8080");
     
     let db_url = std::env::var("DATABASE_URL")?;
     let db_pool: PgPool  = Pool::new(&db_url).await?;
-    let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
-    dbg!(rows);
 
-    let mut app = tide::new();
-    app.at("/").get(|_| async move { Ok("Hello world!") });
+    let mut app: Server<State> = Server::with_state(State  { db_pool });
+
+    app.at("/").get(|req: Request<State>| async move { 
+        let db_pool:&PgPool = &req.state().db_pool;
+
+        let rows = query!("select 1 as one").fetch_one(db_pool).await?;
+        dbg!(rows);
+
+        Ok("Hello world!") 
+    });
+
+    // app.at("/status").get(async move { Ok(rows) });
+    
     app.listen("127.0.0.1:8000").await?;
+
     Ok(())
+}
+#[derive(Debug)]
+struct State {
+    db_pool: PgPool,
 }
 
 #[derive(thiserror::Error, Debug)]
