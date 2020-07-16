c1d6960f8815446963da801daebc4e4e2c139e39
David MacEachern <david.maceachern.tech@gmail.com>
2020-07-16 23:37:18 +0100

    Prints out the response bodyw

--- src/data-collector/src/main.rs
+++ src/data-collector/src/main.rs
@@ -1,13 +1,59 @@
 #![allow(dead_code, unused_macros, unused_variables, unused_mut)]
+
+use async_std::task;
+use dotenv;
+use kv_log_macro as log;
 use std::fs::File;
+use std::env;
 use std::io::prelude::*;
 use std::path::Path;
 // use chrono::{DateTime, FixedOffset, TimeZone, Utc};
 use chrono::Utc;
 
 fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    femme::with_level(femme::LevelFilter::Trace);
+    log::info!("Running Application");
+    let mut id = get_id();
+    let mut secret = get_secret();
+    fetch_data(id, secret);
+    // write_json();
     update_last_modified();
     Ok(())        
+}
+
+fn write_json() {
+    todo!()
+}
+
+fn fetch_data(id: String, secret: String) -> Result<(), surf::Exception>  {
+    println!("id is: {} and secret is  {}", id, secret);
+    task::block_on(async {
+        let uri = "https://testnet-api.phemex.com/v1/md/ticker/24hr/all";
+        let string: String = surf::get(uri).recv_string().await?;
+        println!("{}", string);
+        Ok::<(), surf::Exception>(())
+    })
+}
+
+fn get_id() -> String {
+    let id = env::var("API_ID").is_err();
+    let name = "API_ID";
+    let mut val = match env::var(name) {
+        Err(why) => panic!("couldn't retrieve: {}", why),
+        Ok(val) => val
+    };
+    val
+}
+
+fn get_secret() -> String {
+    let secret = env::var("API_SECRET").is_err();
+    let name = "API_SECRET";
+    let mut val = match env::var(name) {
+        Err(why) => panic!("couldn't retrieve: {}", why),
+        Ok(val) => val    
+    };
+    val
 }
 
 fn update_last_modified() {
